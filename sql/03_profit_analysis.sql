d88931cb3c39eed0eec10963dfa88ec77f840bb3
harshakonakalla96 <harsha.konakalla96@gmail.com>
2026-03-01 13:32:38 +0000

    Update 03_profit_analysis.sql

--- sql/03_profit_analysis.sql
+++ sql/03_profit_analysis.sql
@@ -1,8 +1,4 @@
--- =========================================
--- Profit & Margin Analysis
--- =========================================
-
--- 1. Profit by Sub-Category
+1. Profit by Sub-Category
 SELECT
     `Sub-Category`,
     SUM(Sales) AS Total_Sales,
@@ -12,8 +8,7 @@ FROM harsha_clean
 GROUP BY `Sub-Category`
 ORDER BY Total_Profit ASC;
 
-
--- 2. Loss-Making Products
+2. Loss-Making Products
 SELECT
     `Sub-Category`,
     SUM(Profit) AS Total_Profit
@@ -22,8 +17,7 @@ GROUP BY `Sub-Category`
 HAVING SUM(Profit) < 0
 ORDER BY Total_Profit;
 
-
--- 3. Discount Impact by Category
+3. Discount Impact by Category
 SELECT
     Category,
     AVG(Discount) AS Avg_Discount,
@@ -32,8 +26,7 @@ FROM harsha_clean
 GROUP BY Category
 ORDER BY Avg_Discount DESC;
 
-
--- 4. Total Loss
+4. Total Loss
 SELECT
     SUM(Profit) AS Total_Loss
 FROM harsha_clean
